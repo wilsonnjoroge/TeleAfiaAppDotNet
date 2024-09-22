7dc917e299fde5bbb76057133ee21843fd58d524
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-09-22 08:02:26 +0300

    Update Practitioner.cs

--- TeleAfiaAppDotNet.Domain/UserAggregate/UsersEntities/Practitioner.cs
+++ TeleAfiaAppDotNet.Domain/UserAggregate/UsersEntities/Practitioner.cs
@@ -4,6 +4,7 @@ namespace TeleAfiaAppDotNet.Domain.UserAggregate.UsersEntities
 {
     public class Practitioner : User
     {
+        //Unique properties 
         public string LicenseNumber { get; set; }
         public ICollection<PractitionerType> PractitionerTypes { get; set; } = new List<PractitionerType>();
     }
